5ddebcd95d2c1876b2516badd157547f54f63ec2
Gustavo Alves de Melo Diniz <dinizgustavo085@gmail.com>
2026-06-18 19:29:50 +0000

    fix: atualizando o código de js para validação de acesso

--- Atividade-5/formulario.js
+++ Atividade-5/formulario.js
@@ -5,19 +5,21 @@ function cracharvirtual(){
     let idade = document.getElementById("idade").value
     let cargo = document.getElementById("cargo").value
     let departamento = document.getElementById("departamento").value
-    const acesso = document.getElementById
-    
+    let boolean = document.getElementById("boolean").value
+
 
     console.log("Seu nome é: ",nome)
     console.log("Sua idade: ",idade )
     console.log("Seu Cargo: ",cargo)
     console.log("Seu departamento: ", departamento)
+    console.log("seu acesso: ",boolean)
  
     alert(
         "Funcionario Cadastrado  \n \n Nome do funcionario:  "+ nome +  
-        " \n idade funcionario: "+ idade +  
+        " \n idade funcionario: "+ idade + 
         "\n Cargo funcionario : "+ cargo +  
-        "\n Departamento funcionario: "+ departamento  
+        "\n Departamento funcionario: "+ departamento +
+        "\n Acesso funcionario: "+ boolean
     )
 
 
